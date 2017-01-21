72bc67670ae8926c720e82e74dfc23625999c461
Country.Girls <country.girls@109-b-38fgfwm-m.manorisd.net>
2017-01-21 14:32:31 -0600

    First Commit

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Background: UIImageView!
+    @IBOutlet weak var Banner: UIImageView!
+    @IBOutlet weak var WelcomeBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func Welcomepressed(_ sender: Any) {
+        Background.isHidden=false
+        Banner.isHidden=false
+        WelcomeBtn.isHidden=true
+        
+    }
+    @IBAction func BannerPressed( sender: Any) {
+    Background.isHidden=false
+    Banner.isHidden=true
+    WelcomeBtn.isHidden=true
+ }
 
 }
 
